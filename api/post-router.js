f3b1b1e6c1df0c73dcf05bcf81fbeb6811da49ca
tygedavis <tyge.davis@gmail.com>
2020-01-14 15:03:58 -0700

    POST request for posts works

--- api/post-router.js
+++ api/post-router.js
@@ -6,13 +6,13 @@ const Data = require('../data/db.js');
 router.get('/', (req, res) => {
   Data.find()
     .then(post => {
-      res.status(200).json(post)
+      res.status(200).json(post);
     })
     .catch(err => {
       //console.log(err)
-      res.status(500).json({ error: "The posts information could not be retrieved" })
-    })
-})
+      res.status(500).json({ error: "The posts information could not be retrieved" });
+    });
+});
 
 // ✔ GET -> Posts (Specific ID)
 router.get('/:id', (req, res) => {
@@ -22,15 +22,15 @@ router.get('/:id', (req, res) => {
     .then(post => {
       //console.log(post)
       if(post) {
-        res.status(200).json(post)
+        res.status(200).json(post);
       } else { //Todo Maybe check up on this error message, just returns an empty array
-        res.status(404).json({ message: "The post with that ID does not exist." })
-      }
+        res.status(404).json({ message: "The post with that ID does not exist." });
+      };
     })
     .catch(err => {
       res.status(500).json({ error: "The post information could not be retrieved." })
-    })
-})
+    });
+});
 
 
 // ✔ GET -> Comments (Specific ID)
@@ -48,7 +48,21 @@ router.get('/:id/comments', (req, res) => {
 });
 
 
-//Todo POST -> Posts
+// ✔ POST -> Posts
+router.post('/', (req, res) => {
+
+  Data.insert(req.body)
+    .then(post => {
+      if(!req.body.title || !req.body.contents) { //Todo: Maybe check out why the request still sends even when there is no title or contents
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+      }else
+        console.log(post)
+        res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ error: " There was an error while saving the post to the database." });
+    });
+});
 
 
 //Todo POST -> Comments (Specific ID)
