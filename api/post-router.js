0dc5af7188b67337142de03325b340312c5de998
tygedavis <tyge.davis@gmail.com>
2020-01-14 13:35:24 -0700

    GET to /api/posts works

--- api/post-router.js
+++ api/post-router.js
@@ -2,4 +2,33 @@ const router = require('express').Router();
 
 const Data = require('../data/db.js');
 
+// ✔ GET -> Posts (general)
+router.get('/', (req, res) => {
+  Data.find()
+    .then(post => {
+      res.status(200).json(post)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "The posts information could not be retrieved" })
+    })
+})
+
+//Todo GET -> Posts (Specific ID)
+
+
+//Todo GET -> Comments (Specific ID)
+
+
+//Todo POST -> Posts
+
+
+//Todo POST -> Comments (Specific ID)
+
+
+//Todo DELETE -> Posts (Specific ID)
+
+
+//Todo PUT -> Posts (Specific ID)
+
 module.exports = router
