f26863de19c4cc263945818d31f9e2de81a2b0b0
tygedavis <tyge.davis@gmail.com>
2020-01-14 14:26:45 -0700

    GET for post comments works

--- api/post-router.js
+++ api/post-router.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
       res.status(200).json(post)
     })
     .catch(err => {
-      console.log(err)
+      //console.log(err)
       res.status(500).json({ error: "The posts information could not be retrieved" })
     })
 })
@@ -20,10 +20,10 @@ router.get('/:id', (req, res) => {
 
   Data.findById(id)
     .then(post => {
-      console.log(post)
+      //console.log(post)
       if(post) {
         res.status(200).json(post)
-      } else {
+      } else { //Todo Maybe check up on this error message, just returns an empty array
         res.status(404).json({ message: "The post with that ID does not exist." })
       }
     })
@@ -33,7 +33,19 @@ router.get('/:id', (req, res) => {
 })
 
 
-//Todo GET -> Comments (Specific ID)
+// ✔ GET -> Comments (Specific ID)
+router.get('/:id/comments', (req, res) => {
+  const id = req.params.id;
+
+  Data.findPostComments(id)
+    .then(comments => {
+      //console.log(comments)
+      res.status(200).json(comments)
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The comments info could not be retrieved." });
+    });
+});
 
 
 //Todo POST -> Posts
